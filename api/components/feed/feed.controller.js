63de474c6a80dff3f59cd061630141bb9873f89a
Rigel Glen <rigelglen@gmail.com>
2019-04-02 15:13:41 +0530

    Feed GET requests use query params instead of body

--- api/components/feed/feed.controller.js
+++ api/components/feed/feed.controller.js
@@ -11,35 +11,34 @@ router.get('/getFeedBySearch', validateFeed, getFeedBySearch)
 module.exports = router;
 
 function getFeed(req, res, next) {
-  feedService.getFeed(req.user.sub, req.body.page)
+  feedService.getFeed(req.user.sub, req.query.page)
     .then(feed => feed ? res.json(feed) : res.status(400).json({ message: 'An error occured.' }))
     .catch(err => next(err));
 }
 
 function getFeedByTopic(req, res, next) {
-  feedService.getFeedByTopic(req.body.topicId, req.body.page)
+  feedService.getFeedByTopic(req.query.topicId, req.query.page)
     .then(feed => feed ? res.json(feed) : res.status(400).json({ message: 'An error occured.' }))
     .catch(err => next(err));
 }
 
 function getFeedBySearch(req, res, next) {
-  feedService.getFeedBySearch(req.body.searchString, req.body.page)
+  feedService.getFeedBySearch(req.query.searchString, req.query.page)
     .then(feed => feed ? res.json(feed) : res.status(400).json({ message: 'An error occured.' }))
     .catch(err => next(err));
 }
 
 function validateFeed(req, res, next) {
-
-  if (req.body.searchString && req.body.searchString.length === 0) {
+  if (req.query.searchString && req.query.searchString.length === 0) {
     throw 'Invalid parameter searchString';
   }
 
-  if (req.body.topicId && !ObjectId.isValid(req.body.topicId)) {
+  if (req.query.topicId && !ObjectId.isValid(req.query.topicId)) {
     throw 'Invalid parameter topicId';
   }
 
-  if (req.body.page && !isNaN(parseInt(req.body.page)) && req.body.page > 0) {
-    req.body.page = parseInt(req.body.page);
+  if (req.query.page && !isNaN(parseInt(req.query.page)) && req.query.page > 0) {
+    req.query.page = parseInt(req.query.page);
   }
   else {
     throw 'Invalid parameter page';
