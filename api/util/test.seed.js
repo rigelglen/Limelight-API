321426f17aa894087fe120c771fe5e8299aafee6
Rigel <rigelglen@gmail.com>
2019-04-09 21:00:41 +0530

    Fixed test suite

--- api/util/test.seed.js
+++ api/util/test.seed.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const jwt = require('jsonwebtoken');
 const User = require('./../components/users/user.model');
 const Topic = require('./../components/topic/topic.model');
+
 const bcrypt = require('bcryptjs');
 
 const topicOneId = new ObjectID();
@@ -29,19 +30,54 @@ const userJwts = [jwt.sign({ sub: users[0]._id }, secret), jwt.sign({ sub: users
 
 const topics = [{
     _id: topicOneId,
-    name: 'sports',
+    name: 'politics',
     lastRefreshed: new Date()
 }, {
     _id: topicTwoId,
-    name: 'technology',
+    name: 'technolog',
     lastRefreshed: new Date()
 }];
+
+const categories = [{
+    name: 'world',
+    isCat: true,
+    lastRefreshed: new Date()
+}, {
+    name: 'nation',
+    isCat: true,
+    lastRefreshed: new Date()
+}, {
+    name: 'business',
+    isCat: true,
+    lastRefreshed: new Date()
+}, {
+    name: 'technology',
+    isCat: true,
+    lastRefreshed: new Date()
+}, {
+    name: 'entertainment',
+    isCat: true,
+    lastRefreshed: new Date()
+}, {
+    name: 'sports',
+    isCat: true,
+    lastRefreshed: new Date()
+}, {
+    name: 'science',
+    isCat: true,
+    lastRefreshed: new Date()
+}, {
+    name: 'health',
+    isCat: true,
+    lastRefreshed: new Date()
+}];
+
+
 
 const populateUsers = (done) => {
     User.deleteMany({}).then(() => {
         let userOne = new User(users[0]).save();
         let userTwo = new User(users[1]).save();
-
         return Promise.all([userOne, userTwo]);
     }).then(() => done());
 };
@@ -50,8 +86,9 @@ const populateTopics = (done) => {
     Topic.deleteMany({}).then(() => {
         let topicOne = new Topic(topics[0]).save()
         let topicTwo = new Topic(topics[1]).save();
+        let categoriesPromise = Topic.insertMany(categories);
 
-        return Promise.all([topicOne, topicTwo]);
+        return Promise.all([topicOne, topicTwo, categoriesPromise]);
     }).then(() => done());
 };
 
