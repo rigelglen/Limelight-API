d827fa6f09d99d2af699d9984dcc0f43cd8c2dbe
Rigel <rigelglen@gmail.com>
2019-04-14 00:46:42 +0530

    Fixed mongo data persistence

--- api/core/db.js
+++ api/core/db.js
@@ -6,10 +6,12 @@ mongoose.Promise = global.Promise;
 
 if (process.env.NODE_ENV == 'test') {
     let url = `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGODB_DB_TEST}?authSource=admin`;
+    console.log('db url is ' + url)
     mongoose.connect(url, { useCreateIndex: true, useNewUrlParser: true });
 }
 else {
     let url = `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGODB_DB}?authSource=admin`;
+    console.log('db url is ' + url)
     mongoose.connect(url, { useCreateIndex: true, useNewUrlParser: true });
 }
 module.exports = {
