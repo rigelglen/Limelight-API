6a396e10b88bc86e7c25c28348b0e4c894b9b932
Rigel <rigelglen@gmail.com>
2019-04-14 23:58:01 +0530

    Fixed app service not waiting for database

--- api/core/db.js
+++ api/core/db.js
@@ -3,17 +3,24 @@ const User = require('./../components/users/user.model');
 const Topic = require('./../components/topic/topic.model');
 
 mongoose.Promise = global.Promise;
+const dbOptions = {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    autoReconnect: true,
+    reconnectTries: 5,
+}
+let url = `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGODB_DB}?authSource=admin`;
 
 if (process.env.NODE_ENV == 'test') {
-    let url = `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGODB_DB_TEST}?authSource=admin`;
-    console.log('db url is ' + url)
-    mongoose.connect(url, { useCreateIndex: true, useNewUrlParser: true });
+    url = `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGODB_DB_TEST}?authSource=admin`;
 }
-else {
-    let url = `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGODB_DB}?authSource=admin`;
-    console.log('db url is ' + url)
-    mongoose.connect(url, { useCreateIndex: true, useNewUrlParser: true });
-}
+
+console.log('db url is ' + url)
+
+mongoose.connect(url, dbOptions).catch((ex) => {
+    process.exit(1);
+});
+
 module.exports = {
     User,
     Topic,
--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+until nc -z $MONGO_HOST $MONGO_PORT
+do
+    echo "Waiting for Mongo ($MONGO_HOST:$MONGO_PORT) to start..."
+    sleep 1
+done
+
 if [ $APP_ENV = "production" ]; then
   node index.js;
 else
