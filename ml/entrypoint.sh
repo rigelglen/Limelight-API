3d8a700cdc613b6da430f8231a0a4ff13d51eb78
Rigel <rigelglen@gmail.com>
2019-05-05 18:23:40 +0530

    Reduced spacy memory usage

--- ml/entrypoint.sh
+++ ml/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 if [ $APP_ENV = "production" ]; then
-  gunicorn -w 2 -k uvicorn.workers.UvicornWorker -b $FLASK_HOST:$FLASK_PORT --log-level warning --timeout 240 app:app;
+  # uvicorn --host $FLASK_HOST --port $FLASK_PORT app:app;
+  gunicorn -w 2 -k uvicorn.workers.UvicornWorker -b $FLASK_HOST:3002 --log-level warning --timeout 240 app:app;
 else
   # python app.py
   uvicorn --reload --host $FLASK_HOST --port $FLASK_PORT app:app;
