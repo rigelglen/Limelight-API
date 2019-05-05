0d65f1f91412643040da19be91e76000db08338b
Rigel <rigelglen@gmail.com>
2019-05-05 17:05:47 +0530

    Fixed issue with production server not running

--- ml/entrypoint.sh
+++ ml/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ $APP_ENV = "production" ]; then
-  gunicorn -w 4 -k uvicorn.workers.UvicornWorker -b $FLASK_HOST:$FLASK_PORT --log-level warning app:app;
+  gunicorn -w 2 -k uvicorn.workers.UvicornWorker -b $FLASK_HOST:$FLASK_PORT --log-level warning --timeout 240 app:app;
 else
   # python app.py
   uvicorn --reload --host $FLASK_HOST --port $FLASK_PORT app:app;
